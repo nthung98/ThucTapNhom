b2c56ac3c24714a24b6b486c2b4894747d4f0266
Nguyenduoc98 <dluffy076@gmail.com>
2019-05-07 12:37:58 +0700

    Cat anh

--- ThucTapNhom_XH/ThucTapNhom_XH/Form1.cs
+++ ThucTapNhom_XH/ThucTapNhom_XH/Form1.cs
@@ -17,6 +17,18 @@ namespace ThucTapNhom_XH
             InitializeComponent();
         }
 
+        private void CatAnh(Image image, Image[] images, int index, int numRow, int numCol, int x, int y)
+        {
+            images[index] = new Bitmap(x, y);
+            Graphics objGraphics = Graphics.FromImage(images[index]);
+            // objGraphics = Graphics.FromImage(images[index]);
+            objGraphics.Clear(Color.White);
+            objGraphics.DrawImage(image, new Rectangle(0, 0, x, y),
+                                        new Rectangle(x * (index % numCol), y * (index / numCol), x, y),
+                                        GraphicsUnit.Pixel);
+
+            objGraphics.Flush();
+        }
         private void buttonImageBrowse_Click(object sender, EventArgs e)
         {
 
